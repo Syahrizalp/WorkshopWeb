20bbb591e89df7852f354c337db8f71631784116
Syahrizal Prahastia <syahrizalphas@gmail.com>
2025-09-19 13:12:11 +0700

    Delete Pertemuan 3 directory

--- Pertemuan 3/defer.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('status').textContent = "Script dengan defer dijalankan setelah DOM siap.";
-});
--- Pertemuan 3/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('btn').addEventListener('click', () => {
-  document.getElementById('out').textContent = "JS eksternal berhasil dijalankan!";
-});
