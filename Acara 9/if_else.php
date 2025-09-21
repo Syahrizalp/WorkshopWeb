2a99348192f1db774013ce02b54d3d4f6802ac7c
Syahrizalp <syahrizalphas@gmail.com>
2025-09-21 22:27:19 +0700

    acara 9

--- Acara 9/if_else.php
+++ Acara 9/if_else.php
@@ -5,17 +5,4 @@ if ($umur >= 18) {
 } else { 
     echo "Anda belum dewasa.";
 }
-
-echo "<br>";
-
-$nilai = 75;
-if ($nilai >= 90) {
- echo "Nilai Anda A.";
-} elseif ($nilai >= 80) {
- echo "Nilai Anda B.";
-} elseif ($nilai >= 70) {
- echo "Nilai Anda C.";
-} else {
- echo "Nilai Anda D.";
-}
 ?>
